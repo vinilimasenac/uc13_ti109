8900d9b5e4d1367fe7e26d65d1a0fd0ca257fd58
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-02-27 20:10:33 -0300

    27-02

--- 01-fundamentos/10/index.js
+++ 01-fundamentos/10/index.js
@@ -1,18 +1,26 @@
 
 
 // Exemplo 02 - solicitar ao usuário que informe a primeira nota, em seguinda a segunda nota e após isso o programa calcula a média com arredendamento de duas casa após a virgula
-
+const chalk = require("chalk")
 const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-readline.question("Digite a primeira nota: ", (nota1) => {
-    readline.question("Digite a segunda nota: ", (nota2) => {
-        readline.question("Digite a terceira nota: ", (nota3) => {
-            const media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3
-            console.log(`Notas \n n1:${nota1}\n n2:${nota2}\n n3:${nota3}\n MÉDIA: ${media.toFixed(2)}`)
-            readline.close();
+readline.question("Nome do aluno: ", (nome) => {
+    readline.question("Digite a primeira nota: ", (nota1) => {
+        readline.question("Digite a segunda nota: ", (nota2) => {
+            readline.question("Digite a terceira nota: ", (nota3) => {
+                const media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3
+                console.log(`Notas \n n1:${nota1}\n n2:${nota2}\n n3:${nota3}\n MÉDIA: ${media.toFixed(2)}`)
+
+                if (media >= 5) {
+                    console.log(chalk.bgGreen.black(`Aluno(a) ${nome}, Sua média foi ${media.toFixed(2)}, 😁Parabéns`))
+                } else {
+                    console.log(chalk.bgRed.black(`Aluno(a) ${nome} Sua média foi: ${media.toFixed(2)}, 😥 Não foi dessa vez`))
+                }
+                readline.close();
+            })
         })
     })
 })
